0b9144b72f1f22e82bfb4f26d8b81be40bd3a14d
unknown <fahzzzstd04@gmail.com>
2026-02-12 10:19:58 +0700

    app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Pemerintah Desa') - Desa Resmi</title>
     <meta name="description" content="@yield('description', 'Portal Informasi Desa Resmi')">
-    
-    <!-- @vite('resources/css/app.css') -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
